58663645a4a8d0ca3fd3b7b20ef3eb176a9d7804
everhythm <everhythm@163.com>
2023-10-03 11:53:37 +0800

    mod config net to com

--- client/config.go
+++ client/config.go
@@ -9,7 +9,7 @@ type ServerAddress struct {
 
 var ServerAddressList = []*ServerAddress{
 	{
-		ServerAddress:  "https://game.maj-soul.net",
+		ServerAddress:  "https://game.maj-soul.com", // net 404
 		GatewayAddress: "wss://gateway-hw.maj-soul.net/gateway",
 		GameAddress:    "wss://gateway-hw.maj-soul.com/game-gateway",
 	},
